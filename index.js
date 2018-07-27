a3a333ab49a9848e3715da6f955fa3f6e9a7d987
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-07-27 12:38:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ function theBeatlesPlay(musicians, instruments) {
   
 function johnLennonFacts(i) {
 
-  const facts = [
+  const facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
   ];
   while ( i < facts.length) {
     facts.push(`${facts[i]}!!!`)
